6849a75501054fee05492594aede49275d6a852f
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2024-07-24 09:28:37 -0300

    Update wifi_utils.cpp
    Mejora en la lógica de conexión WiFi y manejo del modo AP
    
    - Se ha ajustado el tiempo de espera y el número de intentos para la conexión WiFi, asegurando que el dispositivo tenga suficiente tiempo para conectarse antes de iniciar el modo AP.
    - Se han añadido mensajes de depuración detallados para cada estado de conexión WiFi, lo que facilita la identificación de problemas específicos de conexión.
    - Se ha mejorado la lógica para alternar entre redes WiFi configuradas, incrementando los intentos antes de pasar a la siguiente red.
    - Se ha refinado el manejo del modo AP para evitar que se inicie demasiado rápido, permitiendo más tiempo para los intentos de conexión WiFi.

--- src/wifi_utils.cpp
+++ src/wifi_utils.cpp
@@ -19,18 +19,17 @@ uint32_t    previousWiFiMillis  = 0;
 uint8_t     wifiCounter         = 0;
 uint32_t    lastBackupDigiTime  = millis();
 
-
 namespace WIFI_Utils {
 
     void checkWiFi() {
         if (backUpDigiMode) {
             uint32_t WiFiCheck = millis() - lastBackupDigiTime;
             if (WiFi.status() != WL_CONNECTED && WiFiCheck >= 15 * 60 * 1000) {
-                Serial.println("*** Stoping BackUp Digi Mode ***");
+                Serial.println("*** Stopping BackUp Digi Mode ***");
                 backUpDigiMode = false;
                 wifiCounter = 0;
             } else if (WiFi.status() == WL_CONNECTED) {
-                Serial.println("*** WiFi Reconnect Success (Stoping Backup Digi Mode) ***");
+                Serial.println("*** WiFi Reconnect Success (Stopping Backup Digi Mode) ***");
                 backUpDigiMode = false;
                 wifiCounter = 0;
             }
@@ -38,7 +37,7 @@ namespace WIFI_Utils {
 
         if (!backUpDigiMode && (WiFi.status() != WL_CONNECTED) && ((millis() - previousWiFiMillis) >= 30 * 1000) && !WiFiAutoAPStarted) {
             Serial.print(millis());
-            Serial.println("Reconnecting to WiFi...");
+            Serial.println(" Reconnecting to WiFi...");
             WiFi.disconnect();
             WiFi.reconnect();
             previousWiFiMillis = millis();
@@ -74,22 +73,57 @@ namespace WIFI_Utils {
             WiFi.disconnect();
             delay(500);
             unsigned long start = millis();
-            show_display("", "Connecting to Wifi:", "", currentWiFi->ssid + " ...", 0);
+            show_display("", "Connecting to WiFi:", "", currentWiFi->ssid + " ...", 0);
             Serial.print("\nConnecting to WiFi '"); Serial.print(currentWiFi->ssid); Serial.println("' ...");
             WiFi.begin(currentWiFi->ssid.c_str(), currentWiFi->password.c_str());
-            while (WiFi.status() != WL_CONNECTED && wifiCounter<myWiFiAPSize) {
-                delay(500);
+
+            int tryDelay = 500;
+            int numberOfTries = 20;  // Ajusta el número de intentos según sea necesario
+
+            while (WiFi.status() != WL_CONNECTED && wifiCounter < myWiFiAPSize && numberOfTries > 0) {
+                delay(tryDelay);
                 #ifdef INTERNAL_LED_PIN
-                    digitalWrite(INTERNAL_LED_PIN,HIGH);
+                    digitalWrite(INTERNAL_LED_PIN, HIGH);
                 #endif
                 Serial.print('.');
-                delay(500);
+                delay(tryDelay);
                 #ifdef INTERNAL_LED_PIN
-                    digitalWrite(INTERNAL_LED_PIN,LOW);
+                    digitalWrite(INTERNAL_LED_PIN, LOW);
                 #endif
-                if ((millis() - start) > 10000){
+
+                numberOfTries--;
+
+                if (WiFi.status() == WL_CONNECTED) {
+                    break;
+                }
+
+                if (numberOfTries == 0 || (millis() - start) > 10000) {
+                    Serial.println();
+                    wl_status_t status = WiFi.status();
+                    switch (status) {
+                        case WL_IDLE_STATUS:
+                            Serial.println("WiFi is in IDLE status");
+                            break;
+                        case WL_NO_SSID_AVAIL:
+                            Serial.println("WiFi SSID not available");
+                            break;
+                        case WL_CONNECT_FAILED:
+                            Serial.println("WiFi connection failed. Wrong password?");
+                            break;
+                        case WL_CONNECTION_LOST:
+                            Serial.println("WiFi connection lost");
+                            break;
+                        case WL_DISCONNECTED:
+                            Serial.println("WiFi is disconnected");
+                            break;
+                        default:
+                            Serial.print("WiFi connection failed with status: ");
+                            Serial.println(status);
+                            break;
+                    }
+
                     delay(1000);
-                    if(myWiFiAPIndex >= (myWiFiAPSize - 1)) {
+                    if (myWiFiAPIndex >= (myWiFiAPSize - 1)) {
                         myWiFiAPIndex = 0;
                         wifiCounter++;
                     } else {
@@ -99,29 +133,30 @@ namespace WIFI_Utils {
                     currentWiFi = &Config.wifiAPs[myWiFiAPIndex];
                     start = millis();
                     Serial.print("\nConnecting to WiFi '"); Serial.print(currentWiFi->ssid); Serial.println("' ...");
-                    show_display("", "Connecting to Wifi:", "", currentWiFi->ssid + " ...", 0);
+                    show_display("", "Connecting to WiFi:", "", currentWiFi->ssid + " ...", 0);
                     WiFi.disconnect();
                     WiFi.begin(currentWiFi->ssid.c_str(), currentWiFi->password.c_str());
                 }
             }
         }
         #ifdef INTERNAL_LED_PIN
-            digitalWrite(INTERNAL_LED_PIN,LOW);
+            digitalWrite(INTERNAL_LED_PIN, LOW);
         #endif
         if (WiFi.status() == WL_CONNECTED) {
             Serial.print("Connected as ");
             Serial.println(WiFi.localIP());
-            show_display("", "     Connected!!", "" , "     loading ...", 1000);
+            show_display("", "     Connected!!", "", "     loading ...", 1000);
+            WiFiConnected = true;
         } else if (WiFi.status() != WL_CONNECTED) {
             startAP = true;
 
             Serial.println("\nNot connected to WiFi! Starting Auto AP");
-            show_display("", " WiFi Not Connected!", "" , "     loading ...", 1000);
+            show_display("", " WiFi Not Connected!", "", "     loading ...", 1000);
         }
         WiFiConnected = !startAP;
         if (startAP) {
             Serial.println("\nNot connected to WiFi! Starting Auto AP");
-            show_display("", "   Starting Auto AP", " Please connect to it " , "     loading ...", 1000);
+            show_display("", "   Starting Auto AP", " Please connect to it ", "     loading ...", 1000);
 
             startAutoAP();
         }
